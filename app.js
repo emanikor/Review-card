2112c3181735717d13920e3edc9b4853c68c440f
emanikor <87859876+emanikor@users.noreply.github.com>
2023-06-02 13:25:44 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -10,21 +10,21 @@ const data = [{
     name: "alexa white",
     image: "images/user-1.png",
     job: "Web designer",
-    text: "Working with UNI-SOKO ecommerce was a game-changer for our web development project. Their expertise and attention to detail resulted in a visually stunning and highly functional website."
+    text: "Working with UNI-SOKO ecommerce designers was an absolute pleasure. Their creative vision and attention to detail brought our website to life. They understood our brand and translated it into a visually stunning design that perfectly captured our message. "
 },
 {
     id: 3,
     name: "collins steve",
     image: "images/user-2.png",
     job: "software engineer",
-    text: "Working with UNI-SOKO ecommerce was a game-changer for our web development project. Their expertise and attention to detail resulted in a visually stunning and highly functional website."
+    text: "I had the privilege of collaborating with software engineer at uni-soko on our e-commerce project, and their expertise and dedication were instrumental in its success. Their deep understanding of software engineering principles and e-commerce best practices enabled us to build a robust and scalable platform"
 },
 {
     id: 4,
     name: "jane ayo",
     image: "images/user-3.png",
     job: "backend developer",
-    text: "Working with UNI-SOKO ecommerce was a game-changer for our web development project. Their expertise and attention to detail resulted in a visually stunning and highly functional website."
+    text: "consistently delivered clean and well-structured code, ensuring the reliability and performance of our application. Their attention to detail and meticulous testing approach helped us identify and resolve potential issues proactively."
 }];
 
 const img = document.getElementById('personimg');
@@ -39,15 +39,36 @@ let currentItem = 0;
 
 // load initial
 window.addEventListener("DOMContentLoaded", function() {
+    showPerson();
+});
+
+// show person
+function showPerson (){
     const item = data[currentItem];
     img.src = item.image;
     author.textContent = item.name;
     job.textContent = item.job;
     info.textContent = item.text;
-    // console.log("user log")
-});
+}
 
+// show next person
 
+nextBtn.addEventListener('click', function(){
+   currentItem++
+   if(currentItem > data.length -1 )
+   currentItem = 0;
+   showPerson(currentItem);
+})
+
+// show prev person
+prevBtn.addEventListener('click', function(){
+    currentItem--;
+    if(currentItem < 0 ){
+        currentItem = data.length-1;
+    }
+    
+    showPerson();
+ })
 
 
 
